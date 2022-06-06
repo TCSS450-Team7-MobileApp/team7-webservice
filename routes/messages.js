b527f0c53ec24677a5634d37e2242fa75db9bc95
Rebekah Parkhurst <cparkh@uw.edu>
2022-06-05 21:37:00 -0700

    returning username

--- routes/messages.js
+++ routes/messages.js
@@ -98,14 +98,14 @@ router.post("/", (request, response, next) => {
     //add the message to the database
     let insert = `INSERT INTO Messages(ChatId, Message, MemberId)
                   VALUES($1, $2, $3) 
-                  RETURNING PrimaryKey AS MessageId, ChatId, Message, MemberId AS email, TimeStamp`
+                  RETURNING PrimaryKey AS MessageId, ChatId, Message, MemberId AS username, TimeStamp`
     let values = [request.body.chatId, request.body.message, request.decoded.memberid]
     pool.query(insert, values)
         .then(result => {
             if (result.rowCount == 1) {
                 //insertion success. Attach the message to the Response obj
                 response.message = result.rows[0]
-                response.message.email = request.decoded.email
+                response.message.username = request.decoded.username
                 //Pass on to next to push
                 next()
             } else {
